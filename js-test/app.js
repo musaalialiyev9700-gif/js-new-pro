010972f3db21849db3c1aba1d9aecadf49e70460
musaalialiyev9700-gif <musaalialiyev9700@gmail.com>
2025-12-24 14:12:46 +0000

    app

--- js-test/app.js
+++ js-test/app.js
@@ -7,14 +7,17 @@ $(document).ready(function () {
 
     $('.btn').click(function (e) {
         e.preventDefault();
-        if (true)  { 
+        if (!$('input').val() == '' || !$('input').val() == ' ')  { 
             let a = b => { 
                 for (let i = 1; i <= b; i++) { $('.minibox').append(`<h2>${b}</h2>`); } 
             }
 
-            let v = f => null ? b : $('.minibox').append(`<h2>${f}</h2>`);  
+            let v = f => Number(f) ? a($('input').val()) : $('.minibox').append(`<h2>${f}</h2>`);  
             v($('input').val());
         }
+
+        $('input').val('');
+        
 
     });
 
