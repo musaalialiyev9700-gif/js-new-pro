ba053c9b4feb3c1d3b777871bf3088377b6d0083
musaalialiyev9700-gif <musaalialiyev9700@gmail.com>
2025-12-29 12:43:16 +0000

    app

--- js-test/app.js
+++ js-test/app.js
@@ -1,19 +1,20 @@
 "use strict";
 $(document).ready(function () {
-
-
+    
 
     $('.btn').click(function (e) {
         e.preventDefault();
         if (!$('input').val().trim() == '') {
             let a = b => {
-                for (let i = 1; i <= b; i++) { $('.minibox').append(`<h2>${b}</h2>`) };
+                for (let i = 1; i <= b; i++) { $('.minibox').append(`<h2>${b}</h2>`); }
             }
-            let v = f => Number(f) ? a($('input').val()) : $('.minibox').append(`<h2>${f}</h2>`)
+            let v = f => Number(f) ? a($('input').val()) : $('.minibox').append(`<h2>${f}</h2>`);
             v($('input').val())
-            
+
         }
+
         $('input').val('')
+
 
     });
 
